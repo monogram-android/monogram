bad9d8c696ad7a6d738c11b0d7176a2c44c90b71
Artur Skubei <41114720+gdlbo@users.noreply.github.com>
2026-03-23 20:00:50 +0300

    change main accent color to telegram blue

--- app/src/main/java/org/monogram/app/ui/theme/Color.kt
+++ app/src/main/java/org/monogram/app/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package org.monogram.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val TelegramBlue80 = Color(0xFF64B5F6)
+val TelegramBlueGrey80 = Color(0xFF81A9CA)
+val TelegramCyan80 = Color(0xFF4DD0E1)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val TelegramBlue40 = Color(0xFF3390EC)
+val TelegramBlueGrey40 = Color(0xFF4C7599)
+val TelegramCyan40 = Color(0xFF00ACC1)
--- app/src/main/java/org/monogram/app/ui/theme/Theme.kt
+++ app/src/main/java/org/monogram/app/ui/theme/Theme.kt
@@ -6,21 +6,22 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = TelegramBlue80,
+    secondary = TelegramBlueGrey80,
+    tertiary = TelegramCyan80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = TelegramBlue40,
+    secondary = TelegramBlueGrey40,
+    tertiary = TelegramCyan40
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),

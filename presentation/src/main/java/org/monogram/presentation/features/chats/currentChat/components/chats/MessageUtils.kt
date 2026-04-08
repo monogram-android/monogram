3ea8b56f9140c684b5a26ea55396f84fa8a84b0c
Andro_Dev <andromaster436@gmail.com>
2026-04-08 18:48:59 +0300

    fix: potential locale crash

--- presentation/src/main/java/org/monogram/presentation/features/chats/currentChat/components/chats/MessageUtils.kt
+++ presentation/src/main/java/org/monogram/presentation/features/chats/currentChat/components/chats/MessageUtils.kt
@@ -57,7 +57,7 @@ fun formatFileSize(size: Long, isDownloading: Boolean, downloadProgress: Float):
         if (value <= 0) return "0 B"
         val digitGroups = (log10(value) / log10(1024.0)).toInt()
         return String.format(
-            Locale.getDefault(),
+            Locale.US,
             "%.1f %s",
             value / 1024.0.pow(digitGroups.toDouble()),
             units[digitGroups]

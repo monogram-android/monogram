87a51fa48902c20906ec443463a377c13df1133b
iWisp360 <iwisp360@tutamail.com>
2026-04-08 20:57:37 -0400

    fix flake with wrong dependencies and format

--- flake.nix
+++ flake.nix
@@ -1,78 +1,75 @@
 {
-    description = "Native Telegram client for Android based on TDLib.";
+  description = "Native Telegram client for Android based on TDLib.";
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-        flake-parts.url = "github:hercules-ci/flake-parts";
-        systems.url = "github:nix-systems/default";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
 
-    outputs =
-        inputs:
-        inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-            systems = import inputs.systems;
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
 
-            perSystem =
-                { pkgs, system, ... }:
-                let
-                    androidComposition =
-                        pkgs:
-                        (pkgs.androidenv.composeAndroidPackages {
-                            platformVersions = [
-                                "35"
-                                "36"
-                            ];
+      perSystem =
+        { pkgs, system, ... }:
+        let
+          androidComposition =
+            pkgs:
+            (pkgs.androidenv.composeAndroidPackages {
+              platformVersions = [
+                "35"
+              ];
 
-                            buildToolsVersions = [
-                                "35.0.0"
-                            ];
+              buildToolsVersions = [
+                "35.0.0"
+              ];
 
-                            cmakeVersions = [
-                                "3.22.1"
-                            ];
+              cmakeVersions = [
+                "3.22.1"
+              ];
 
-                            ndkVersions = [
-                                "27.0.12077973"
-                            ];
+              ndkVersions = [
+                "27.0.12077973"
+                "28.2.13676358"
+              ];
 
-                            abiVersions = [
-                                "armeabi-v7a"
-                                "arm64-v8a"
-                                "x86_64"
-                            ];
+              abiVersions = [
+                "armeabi-v7a"
+                "arm64-v8a"
+                "x86_64"
+              ];
 
-                            systemImageTypes = [ "google_apis_playstore" ];
-                            includeNDK = true;
-                            includeCmake = true;
-                            includeExtras = [ "extras;google;auto" ];
-                        }).androidsdk;
-                in
-                {
-                    _module.args.pkgs = import inputs.nixpkgs {
-                        inherit system;
-                        config = {
-                            android_sdk.accept_license = true;
-                            allowUnfree = true;
-                        };
-                    };
+              systemImageTypes = [ "google_apis_playstore" ];
+              includeNDK = true;
+              includeCmake = true;
+              includeSources = true;
+              includeExtras = [ "extras;google;auto" ];
+            }).androidsdk;
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            config = {
+              android_sdk.accept_license = true;
+              allowUnfree = true;
+            };
+          };
 
-                    devShells.default =
-                        (pkgs.buildFHSEnv {
-                            name = "monogram-sdk-env";
+          devShells.default = pkgs.mkShell {
+            name = "monogram-sdk";
 
-                            targetPkgs = pkgs: [
-                                (androidComposition pkgs)
-                                pkgs.glibc
-                                pkgs.jdk21
-                            ];
+            nativeBuildInputs = [
+              (androidComposition pkgs)
+              pkgs.glibc
+              pkgs.jdk21
+              pkgs.android-studio
+            ];
 
-                            profile = ''
-                                export JAVA_HOME="/usr/lib/openjdk"
-                                export ANDROID_HOME="/usr/libexec/android-sdk"
-                            '';
-
-                            runScript = "bash";
-                        }).env;
-                };
+            JAVA_HOME = "${pkgs.jdk21}";
+            ANDROID_HOME = "${androidComposition pkgs}/libexec/android-sdk";
+          };
         };
+    };
 }

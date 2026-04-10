f804b5a9953a420b1e0b3b01b57ff1b41274c9fa
iWisp360 <iwisp360@tutamail.com>
2026-04-10 10:20:46 -0400

    exclude apk release workflow from this branch

--- flake.nix
+++ flake.nix
@@ -1,103 +1,78 @@
 {
-  description = "Native Telegram client for Android based on TDLib.";
+    description = "Native Telegram client for Android based on TDLib.";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    systems.url = "github:nix-systems/default";
-  };
-
-  outputs =
-    inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
-
-      perSystem =
-        { pkgs, system, ... }:
-        let
-          actualBuildTools = "36.0.0";
-          actualPlatform = "36";
-          actualNdk = "28.2.13676358";
-          androidComposition =
-            pkgs:
-            (pkgs.androidenv.composeAndroidPackages {
-              platformVersions = [ actualPlatform ];
-              buildToolsVersions = [ actualBuildTools ];
-              ndkVersions = [ actualNdk ];
-              cmakeVersions = [ "3.22.1" ];
-              abiVersions = [
-                "armeabi-v7a"
-                "arm64-v8a"
-                "x86_64"
-              ];
-
-              systemImageTypes = [ "google_apis_playstore" ];
-              includeNDK = true;
-              includeCmake = true;
-              includeSources = true;
-              includeExtras = [ "extras;google;auto" ];
-            }).androidsdk;
-        in
-        {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            config = {
-              android_sdk.accept_license = true;
-              allowUnfree = true;
-            };
-          };
-
-          devShells.default = pkgs.mkShell {
-            name = "monogram-sdk";
-
-            nativeBuildInputs = [
-              (androidComposition pkgs)
-              pkgs.glibc
-              pkgs.jdk21
-              pkgs.android-studio
-            ];
-
-            JAVA_HOME = "${pkgs.jdk21}";
-            ANDROID_HOME = "${androidComposition pkgs}/libexec/android-sdk";
-          };
-
-          apps = {
-            buildRelease = {
-              type = "app";
-              program = "${pkgs.writeShellScriptBin "buildRelease" ''
-                if ! ${pkgs.git}/bin/git submodule update --init --recursive; then
-                  echo "failed getting submodules"
-                fi
-
-                if ! [ -f local.properties ] || ! [ -f app/google-services.json ]; then
-                  echo "Warning: local.properties and/or app/google-services.json aren't found,"
-                  echo "build will most likely fail"
-                fi
-
-                export ANDROID_HOME="${androidComposition pkgs}/libexec/android-sdk/"
-                export ANDROID_SDK_ROOT="${androidComposition pkgs}/libexec/android-sdk/"
-                export ANDROID_NDK_HOME="$ANDROID_HOME/ndk/${actualNdk}/"
-                export GRADLE_OPTS="-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidComposition pkgs}/libexec/android-sdk/build-tools/${actualBuildTools}/aapt2"
-                export JAVA_HOME="${pkgs.jdk21}"
-
-                cd presentation/src/main/cpp/
-                if ! [ -d libvpx_build ]; then
-                  sed -e "s|#!/bin/bash|#!${pkgs.bash}/bin/bash|g" \
-                    -e "s|make clean|${pkgs.gnumake}/bin/make clean|g" \
-                    -e "s|make -j|${pkgs.gnumake}/bin/make -j|g" build.sh >new_build.sh
-                  chmod +x new_build.sh
-                  if ! ${pkgs.bash}/bin/bash new_build.sh; then
-                    echo "libvpx build failed"
-                    exit 1
-                  fi
-                  rm new_build.sh
-                fi 
-
-                cd ../../../..
-                ./gradlew assembleRelease
-              ''}/bin/buildRelease";
-            };
-          };
-        };
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        flake-parts.url = "github:hercules-ci/flake-parts";
+        systems.url = "github:nix-systems/default";
     };
+
+    outputs =
+        inputs:
+        inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+            systems = import inputs.systems;
+
+            perSystem =
+                { pkgs, system, ... }:
+                let
+                    androidComposition =
+                        pkgs:
+                        (pkgs.androidenv.composeAndroidPackages {
+                            platformVersions = [
+                                "35"
+                                "36"
+                            ];
+
+                            buildToolsVersions = [
+                                "35.0.0"
+                            ];
+
+                            cmakeVersions = [
+                                "3.22.1"
+                            ];
+
+                            ndkVersions = [
+                                "27.0.12077973"
+                            ];
+
+                            abiVersions = [
+                                "armeabi-v7a"
+                                "arm64-v8a"
+                                "x86_64"
+                            ];
+
+                            systemImageTypes = [ "google_apis_playstore" ];
+                            includeNDK = true;
+                            includeCmake = true;
+                            includeExtras = [ "extras;google;auto" ];
+                        }).androidsdk;
+                in
+                {
+                    _module.args.pkgs = import inputs.nixpkgs {
+                        inherit system;
+                        config = {
+                            android_sdk.accept_license = true;
+                            allowUnfree = true;
+                        };
+                    };
+
+                    devShells.default =
+                        (pkgs.buildFHSEnv {
+                            name = "monogram-sdk-env";
+
+                            targetPkgs = pkgs: [
+                                (androidComposition pkgs)
+                                pkgs.glibc
+                                pkgs.jdk21
+                            ];
+
+                            profile = ''
+                                export JAVA_HOME="/usr/lib/openjdk"
+                                export ANDROID_HOME="/usr/libexec/android-sdk"
+                            '';
+
+                            runScript = "bash";
+                        }).env;
+                };
+        };
 }

821f6387e3544ee400652cce9578515e4c6f19de
Artur Skubei <41114720+gdlbo@users.noreply.github.com>
2026-04-15 18:18:14 +0300

    Annotate verified IDs and add new users
    Add inline comments to VERIFIED_CHAT_IDS and VERIFIED_USER_IDS to document Telegram handles and roles for readability and attribution. Also add two new verified user IDs (1374434073 - @the8055u, 646667177 - @ramedon)

--- data/src/main/java/org/monogram/data/mapper/VerifiedOverrides.kt
+++ data/src/main/java/org/monogram/data/mapper/VerifiedOverrides.kt
@@ -1,18 +1,20 @@
 package org.monogram.data.mapper
 
 private val VERIFIED_CHAT_IDS = setOf(
-    -1003615282448L,
-    -1003768707135L,
-    -1003566234286L,
-    -1001270834900L,
-    -1001336987857L
+    -1003615282448L, // @monogram_discuss
+    -1003768707135L, // @monogram_android
+    -1003566234286L, // @monogram_apks
+    -1001270834900L, // private chat
+    -1001336987857L // @LBOGD
 )
 
 private val VERIFIED_USER_IDS = setOf(
-    453024846L,
-    665275967L,
-    1250144551L,
-    454755463L
+    453024846L, // @gdlbo - Lead developer
+    665275967L, // @Rozetka_img - Lead developer
+    1250144551L, // @toxyxd - Contributor
+    454755463L, // @recodius - Contributor
+    1374434073L, // @the8055u - Logo designer
+    646667177L // @ramedon - Big thanks
 )
 
 fun isForcedVerifiedChat(chatId: Long): Boolean = chatId in VERIFIED_CHAT_IDS
